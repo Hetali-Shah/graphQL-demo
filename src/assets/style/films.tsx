bccfb8f96038afed7d676d59b8847828532a0d44
Hetali-Shah <hetali1311.shah@gmail.com>
2023-03-25 16:32:55 +0530

    WIP: UI of films

--- src/assets/style/films.tsx
+++ src/assets/style/films.tsx
@@ -1,11 +1,10 @@
 import {StyleSheet} from 'react-native';
 import StyleConfig from './config';
-import { WINDOW } from "../../common/global";
 
 export default StyleSheet.create({
   container: {
-    flex:1
-  } ,
+    height:'100%'
+  },
   mainView:{
     flex:1,
     marginVertical: StyleConfig.countPixelRatio(5),
@@ -27,5 +26,71 @@ export default StyleSheet.create({
     flex:1,
     margin: StyleConfig.countPixelRatio(5),
     padding: StyleConfig.countPixelRatio(5),
-  }
+  },
+
+
+  imageBack:{
+    height:'80%',
+    width:'100%'
+  },
+  safeAreaMainView:{
+    flex:1,
+    padding:StyleConfig.countPixelRatio(15),
+    paddingHorizontal:StyleConfig.countPixelRatio(5),
+  },
+  backButton: {
+    paddingTop: StyleConfig.countPixelRatio(20),
+    flexDirection:'row',
+    justifyContent:'space-between'
+  },
+  bookTitle:{
+    textAlign:'center',
+    fontSize: StyleConfig.fontSizeH2,
+    fontFamily: StyleConfig.bold,
+    color: StyleConfig.white,
+  },
+  dateView:{
+    width:"100%",
+    justifyContent:'center',
+    paddingTop:StyleConfig.countPixelRatio(30),
+    alignItems:'center',
+    flexDirection:'row'
+  },
+  dateNestedView:{
+    paddingHorizontal:StyleConfig.countPixelRatio(5),
+    flexDirection:'row',
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  dateText: {
+    fontSize:StyleConfig.fontSizeSubParagraph,
+    color:StyleConfig.white,
+    padding:StyleConfig.countPixelRatio(5),
+    textAlign:'center',
+  },
+  openingCrawlMainView: {
+    paddingHorizontal:StyleConfig.countPixelRatio(10),
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+  },
+  openingCrawlIconView:{
+    flex:0.4,
+    flexDirection:'row'
+  },
+  openingCrawTitleText: {
+    marginTop:'auto',
+    flex:0.6,
+    fontSize:StyleConfig.fontSizeFieldTitle,
+    color:StyleConfig.white,
+    fontFamily:StyleConfig.bold,
+    padding:StyleConfig.countPixelRatio(5)
+  },
+  openingCrawText: {
+    fontSize:StyleConfig.fontSizeParagraph,
+    color:StyleConfig.white,
+    fontFamily:StyleConfig.regular,
+    padding:StyleConfig.countPixelRatio(5),
+    lineHeight: StyleConfig.countPixelRatio(20)
+  },
 });
--- src/screens/Films.tsx
+++ src/screens/Films.tsx
@@ -1,11 +1,17 @@
-import React, { useEffect } from "react";
-import { Text, View } from "react-native";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { ImageBackground, StatusBar, Text, SafeAreaView, View, TouchableOpacity } from "react-native";
 import { useQuery } from "@apollo/client";
 import {GET_FILM_BY_ID} from "../graphQL"
 import NoDataFound from "../components/NoDataFound";
 import FilmsStyle from "../assets/style/films";
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import LinearGradient from "react-native-linear-gradient"
 import {APP_SCREEN, RootStackParamList} from '../navigation/screenTypes';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import Entypo from "react-native-vector-icons/Entypo";
+import Feather from 'react-native-vector-icons/Feather';
+import { NavigationService } from "../navigation/navigationServices";
 
 export type FilmDetailsProps = NativeStackScreenProps<
   RootStackParamList,
@@ -20,9 +26,21 @@ const FilmDetailsComponent: React.FC<FilmDetailsProps> = (
     variables: { id },
   });
 
+  const [textShown, setTextShown] = useState(false)
+  const [lengthMore,setLengthMore] = useState(false); //to show the "Read more & Less Line"
+  const toggleNumberOfLines = () => { //To toggle the show text or hide it
+    setTextShown(!textShown);
+  }
+
+  const onTextLayout = useCallback(e =>{
+    setLengthMore(e.nativeEvent.lines.length >=4); //to check the text is more than 4 lines or not
+    // console.log(e.nativeEvent);
+  },[]);
+
   useEffect(() => {
     navigation.setOptions({
-      headerTitle: params.headerName
+      headerShown:false,
+      headerTransparent:true
     });
   }, []);
 
@@ -41,15 +59,83 @@ const FilmDetailsComponent: React.FC<FilmDetailsProps> = (
   const { title, director, releaseDate, openingCrawl, created, producers } = data.film;
 
   return (
-    <View style={FilmsStyle.mainView}>
-      <View style={FilmsStyle.filmTextView}>
-        <Text style={FilmsStyle.filmText}>Title : {title}</Text>
-        <Text style={FilmsStyle.filmText}>Director : {director}</Text>
-        <Text style={FilmsStyle.filmText}>Producers : {producers}</Text>
-        <Text style={FilmsStyle.filmText}>ReleaseDate : {new Date(releaseDate).toISOString().substring(0, 10)}</Text>
-        <Text style={FilmsStyle.filmText}>Created : {new Date(created).toISOString().substring(0, 10)}</Text>
-        <Text style={FilmsStyle.filmText}>OpeningCrawl : {openingCrawl}</Text>
-      </View>
+    <View style={{ flex: 1 }}>
+      <StatusBar translucent backgroundColor='transparent' />
+      <ImageBackground
+        style={FilmsStyle.imageBack}
+        source={{uri: `https://picsum.photos/200/200?random=${Math.random()}`}}
+      >
+        <LinearGradient
+          colors={['#00000000', '#000000']}
+          style={{height : '80%', width : '100%'}}>
+          <SafeAreaView style={{flex:1}}>
+            <View style={FilmsStyle.safeAreaMainView}>
+              <View style={FilmsStyle.backButton}>
+                <TouchableOpacity onPress={() => NavigationService.navigate(APP_SCREEN.All_FILMS)} >
+                  <Icon name="arrow-back" size={25} color="#fff" />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <Feather name="heart" size={25} color="#fff" />
+                </TouchableOpacity>
+              </View>
+
+              <View style={{ paddingTop: 100}}>
+                <Text style={FilmsStyle.bookTitle}>{title}</Text>
+                <View style={FilmsStyle.dateView}>
+                  <View style={FilmsStyle.dateNestedView}>
+                    <FontAwesome5 name="calendar-alt" size={10} color="#fff" />
+                    <Text style={FilmsStyle.dateText}>{new Date(releaseDate).toISOString().substring(0, 10)}</Text>
+                  </View>
+                  <Text style={{fontSize: 20}}>|</Text>
+                  <View style={FilmsStyle.dateNestedView}>
+                    <FontAwesome5 name="calendar-alt" size={10} color="#fff" />
+                    <Text style={FilmsStyle.dateText}>2020</Text>
+                  </View>
+                  <Text style={{fontSize: 20}}>|</Text>
+                  <View style={FilmsStyle.dateNestedView}>
+                    <Text style={FilmsStyle.dateText}>{director}</Text>
+                  </View>
+                </View>
+                <View style={{
+                  height:'68%',
+                  display:'flex'
+                }}>
+                  <View style={{marginTop:'auto'}}>
+                  <View style={FilmsStyle.openingCrawlMainView}>
+                    <Text style={FilmsStyle.openingCrawTitleText}>OpeningCrawl</Text>
+                    <View style={FilmsStyle.openingCrawlIconView}>
+                      <View style={{ flex:0.7, alignItems:'flex-end'}}>
+                        <Entypo name="download" size={20} color="red" />
+                      </View>
+
+                      <View style={{flex:0.3, alignItems:'flex-end'}}>
+                        <FontAwesome5 name="share" size={20} color="red" />
+                      </View>
+                    </View>
+                  </View>
+
+                  <View style={FilmsStyle.openingCrawlMainView}>
+                    <Text style={FilmsStyle.openingCrawText}
+                          onTextLayout={onTextLayout}
+                          numberOfLines={textShown ? undefined : 3.8}
+                    >{openingCrawl.replace(/[\r\n]/gm, '')}
+                    </Text>
+                    {/*{
+                    lengthMore ? <Text
+                        onPress={toggleNumberOfLines}
+                        style={{ lineHeight: 21, marginTop: 10 }}>{textShown ? 'Read less' : 'Read more'}</Text>
+                      :null
+                  }*/}
+                  </View>
+                  </View>
+                </View>
+
+
+              </View>
+            </View>
+          </SafeAreaView>
+        </LinearGradient>
+      </ImageBackground>
     </View>
   )
 }
